71e18ef5bb1063f516a8fee16fad7903d66b99a5
Avraam Georgiadis <me@visualabra.com>
2024-10-13 10:05:58 +0200

    Quick fix for double entries
    I had to add a weird boolean flag to prevent the input field triggering on_value_changed

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import streamlit as st
 existingTasksKey = "existingTasks"
 inputFieldKey = "newTaskInputField"
+disableOnChangeKey = "disable_on_change"
 # Initialize session state for tasks if not already initialized
 if existingTasksKey not in st.session_state:
     st.session_state[existingTasksKey] = []
+if disableOnChangeKey not in st.session_state:
+    st.session_state[disableOnChangeKey] = False
+
+def ClearInputFieldWithoutTriger():
+    st.session_state[disableOnChangeKey] = True
+    st.session_state[inputFieldKey] = ""
+    st.session_state[disableOnChangeKey] = False
 
 # Function to add a task
-def add_task():
+def AddNewTask():
+    if st.session_state[disableOnChangeKey]:
+        return
     new_task = st.session_state[inputFieldKey]
     if new_task:  # Check if the input is not empty
         st.session_state[existingTasksKey].append(new_task)  # Add the task to the list
-        del st.session_state[inputFieldKey]
+        ClearInputFieldWithoutTriger()
+
+
     else:
         st.error("Input can't be empty")  # Show error if the input is empty
 
@@ -27,6 +39,6 @@ if st.session_state[existingTasksKey]:
 
 
 # Input box to enter a new task
-st.text_input('Enter a new task', key=inputFieldKey, on_change=add_task)
+inputField = st.text_input('Enter a new task', key=inputFieldKey, on_change=AddNewTask)
 # Button to add a task with a callback to trigger the function
-st.button('Add Task', on_click=add_task)
+st.button('Add Task', on_click=AddNewTask)
